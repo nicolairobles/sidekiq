b452736f9ccfefb5bc00955924ce3d907aee46df
Nicolai Robles <nicolai.robles@gmail.com>
2016-02-19 15:46:09 -0500

    fail

--- sidekiq_newsletter/app/mailers/recipient_mailer.rb
+++ sidekiq_newsletter/app/mailers/recipient_mailer.rb
@@ -1,9 +1,8 @@
-class NewsletterMailer < ApplicationMailer
+class RecipientMailer < ApplicationMailer
   default from: "sidekiq.test.ga@gmail.com"
-
+ 
   def newsletter(recipient)
     @recipients = recipient["email"]
-    mail(to: @recipients_email, subject: "testing our emails")
+    mail(to: @recipients, subject: "testing our emails")
   end
-
 end
